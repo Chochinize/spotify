86f0942f8c77b77e7076d8ca4fcbd95cfcd84ff1
chochiniz <chochinize@gmail.com>
2021-12-10 16:29:07 +0100

    hook fix

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -6,7 +6,6 @@ import SpotifyWebApi from "spotify-web-api-node";
 const spotifyApi = new SpotifyWebApi({
     clientId:process.env.NEXT_PUBLIC_CLIENT_ID,
     clientSecret:process.env.NEXT_PUBLIC_CLIENT_SECRET,
-    // redirectUri:process.env.REDIRECT_URI,
 });
 function useSpotify() {
     const {data:session,status} = useSession();
